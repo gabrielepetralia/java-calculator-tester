73c5eb8b1e676e26a42cb5708f9126f71a059172
gabrielepetralia <gabri.petralia03@gmail.com>
2023-09-29 12:48:26 +0200

    create class Calculator

---
+++ src/main/java/org/java/Calculator.java
@@ -0,0 +1,24 @@
+package org.java;
+
+public class Calculator {
+	
+	public float add(float num1, float num2) {
+		return num1 + num2;
+	}
+	
+	public float subtract(float num1, float num2) {
+		return num1 - num2;
+	}
+	
+	public float divide(float num1, float num2) throws Exception {
+		if(num2 == 0) {
+			throw new Exception("Impossibile dividere per 0");
+		}
+		
+		return num1 / num2;
+	}
+	
+	public float multiply(float num1, float num2) {
+		return num1 * num2;
+	}
+}
